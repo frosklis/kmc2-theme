3677688a5a2c04aaf0cd150a578c10af7b8d1a94
Claudio Noguera <claudio.noguera@gmail.com>
2012-12-30 23:13:47 +0100

    añadido home.php, falta devolver index.php a como estaba antes

---
+++ home.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+			
+			<div id="content">
+			
+				<div id="inner-content" class="wrap clearfix">
+			
+				    <div id="main" class="twelvecol first clearfix" role="main">
+					<?php if ( is_active_sidebar( 'cover1' ) ) : ?>
+
+						<?php dynamic_sidebar( 'cover1' ); ?>
+
+					<?php else : ?>
+
+						<!-- This content shows up if there are no widgets defined in the backend. -->
+						
+						<div class="alert help">
+							<p><?php _e("Please activate some Widgets.", "kmc2theme");  ?></p>
+						</div>
+
+					<?php endif; ?>
+				    </div> <!-- end #main -->
+    
+				    <?php // No sidebar get_sidebar(); ?>
+				    
+				</div> <!-- end #inner-content -->
+    
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
