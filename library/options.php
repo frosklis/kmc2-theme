489da0504284796515a566573cedba97425815a0
Claudio Noguera <claudio.noguera@gmail.com>
2012-12-31 01:08:25 +0100

    rm_script --> rm_scripts

--- library/options.php
+++ library/options.php
@@ -99,7 +99,7 @@ add_menu_page($themename, $themename, 'administrator', basename(__FILE__), 'myth
 function mytheme_add_init() {
 	$file_dir=get_bloginfo('template_directory');  
 	wp_enqueue_style("functions", $file_dir."/library/css/options.css", false, "1.0", "all"); 
-	wp_enqueue_script("rm_script", $file_dir."/library/js/rm_script.js", false, "1.0");  
+	wp_enqueue_script("rm_script", $file_dir."/library/js/rm_scripts.js", false, "1.0");  
 } 
 
 
