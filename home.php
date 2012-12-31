0d1822ff665bea2f4c0ee4d15019f92bb48d5dd1
Claudio Noguera <claudio.noguera@gmail.com>
2012-12-31 02:16:08 +0100

    preparando el slideshow

--- home.php
+++ home.php
@@ -4,23 +4,9 @@
 			
 				<div id="inner-content" class="wrap clearfix">
 			
-				    <div id="main" class="twelvecol first clearfix" role="main">
-					<?php if ( is_active_sidebar( 'cover1' ) ) : ?>
-
-						<?php dynamic_sidebar( 'cover1' ); ?>
-
-					<?php else : ?>
-
-						<!-- This content shows up if there are no widgets defined in the backend. -->
-						
-						<div class="alert help">
-							<p><?php _e("Please activate some Widgets.", "kmc2theme");  ?></p>
-						</div>
-
-					<?php endif; ?>
-				    </div> <!-- end #main -->
-    
-				    <?php // No sidebar get_sidebar(); ?>
+					<div id="cover_banner" class="home-slideshow">
+						<h1 align="center">There will be a slideshow here</h1>
+					</div>
 				    
 				</div> <!-- end #inner-content -->
     
