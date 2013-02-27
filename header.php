28da972a27c3900c1d544e0a98e45cdc8439d987
Claudio Noguera <claudio.noguera@gmail.com>
2013-02-27 12:36:07 +0100

    añadir un endif en el header.php

--- header.php
+++ header.php
@@ -44,6 +44,7 @@
 							if (have_posts()) : while (have_posts()) : the_post();
 							the_title();
 							endwhile;
+							endif;
 						} else {
 						?>
 							<a href="<?php echo home_url(); ?>" rel="nofollow">km c<sup>2</sup><?php //bloginfo('name'); ?></a></span>
