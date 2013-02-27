cfb01d23bf0dee422c5a3d84d7d7fac6676f080f
Claudio Noguera <claudio.noguera@gmail.com>
2013-02-27 12:11:55 +0100

    cambiar el footer

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
     					<?php kmc2_footer_links(); ?>
 	                </nav>
 	                		
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php //bloginfo('name'); ?><a href="<?php echo home_url(); ?>" rel="nofollow">km c<sup>2</sup><?php //bloginfo('name'); ?></a>.</p>
+					<p class="source-org copyright"><a href="<?php echo home_url(); ?>" rel="nofollow">&copy; <?php echo date('Y'); ?> <?php //bloginfo('name'); ?>km c<sup>2</sup><?php //bloginfo('name'); ?>.</a></p>
 				
 				</div> <!-- end #inner-footer -->
 				
