43d3c56c0beb89931bafd5b341d1b77019e872a0
Claudio Noguera <claudio.noguera@gmail.com>
2013-03-05 10:46:35 +0100

    quitar los captions de la portada

--- home.php
+++ home.php
@@ -11,15 +11,15 @@
 							<ul class="slides">
 							  <li>
 							  	<img src="./wp-content/themes/kmc2-theme/mockups/_DSC6580.jpg" />
-							  	<p class="flex-caption">Esto es una foto.</p>
+							  	<p class="flex-caption"><!-- Esto es una foto. --></p>
 							  </li>
 							  <li>
 							  	<img src="./wp-content/themes/kmc2-theme/mockups/_DSC6259.jpg" />
-							  	<p class="flex-caption">Esto es otra foto.</p>
+							  	<p class="flex-caption"><!-- Esto es otra foto. --></p>
 							  </li>
 							  <li>
 							  	<img src="./wp-content/themes/kmc2-theme/mockups/_DSC4849.jpg" />
-							  	<p class="flex-caption">Y una más.</p>
+							  	<p class="flex-caption"><!-- Y una más. --></p>
 							  </li>
 							</ul>
 						  </div>
