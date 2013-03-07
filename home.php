2d1ad5cbb3f0333a5d74b54f85669fc2951d0525
Claudio Noguera <claudio.noguera@gmail.com>
2013-03-07 16:01:44 +0100

    ahora las imágenes de la portada se seleccionan directamente desde el blog

--- home.php
+++ home.php
@@ -1,26 +1,25 @@
 <?php get_header(); ?>
 			
 			<div id="content">
-			
 				<div id="inner-content" class="wrap clearfix">
-			
 					<!--Cover banner-->
 					<div id="cover_banner" class="home-slideshow">
 						<div class="flex-container">
 						  <div class="flexslider">
 							<ul class="slides">
-							  <li>
-							  	<img src="./wp-content/themes/kmc2-theme/mockups/_DSC6580.jpg" />
-							  	<p class="flex-caption"><!-- Esto es una foto. --></p>
-							  </li>
-							  <li>
-							  	<img src="./wp-content/themes/kmc2-theme/mockups/_DSC6259.jpg" />
-							  	<p class="flex-caption"><!-- Esto es otra foto. --></p>
-							  </li>
-							  <li>
-							  	<img src="./wp-content/themes/kmc2-theme/mockups/_DSC4849.jpg" />
-							  	<p class="flex-caption"><!-- Y una más. --></p>
-							  </li>
+			
+								<?php			
+								$media_items = get_attachments_by_media_tags('media_tags=portada');
+								
+								foreach ($media_items as $imagen) { ?>
+									<li>
+										<img src=<?php echo($imagen->guid); ?>>
+										<p class="flex-caption"><?php // var_dump($imagen); ?></p>
+									</li> 
+								<?php
+								} // foreach
+								?>
+
 							</ul>
 						  </div>
 						</div>
