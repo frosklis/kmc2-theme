76fa34412e234d73e767ec8eed28289aa05e4a95
Claudio Noguera <claudio.noguera@gmail.com>
2013-06-09 14:39:37 +0200

    Añadir link de editar el post

--- functions.php
+++ functions.php
@@ -168,9 +168,10 @@ function display_posts ($list_of_posts = null) {
             <header class="article-header">
             
                 <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-    <p class="byline vcard"><?php
-    printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'kmc2theme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), kmc2_get_the_author_posts_link(), get_the_category_list(', '));
-    ?></p>
+                <p class="byline vcard"><?php
+                printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'kmc2theme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), kmc2_get_the_author_posts_link(), get_the_category_list(', '));
+                ?></p>
+                <p><?php edit_post_link( $link, $before, $after, $id ); ?></p>
         
             </header> <!-- end article header -->
 
