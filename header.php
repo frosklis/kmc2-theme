d042039b1a6c2719bee2cff2910f554cd60a0c7b
Claudio Noguera <claudio.noguera@gmail.com>
2013-06-11 22:57:16 +0200

    limpiar un poco el header

--- header.php
+++ header.php
@@ -1,5 +1,4 @@
 <!doctype html>  
-
 <!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
 <!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
@@ -10,22 +9,17 @@
 		
 		<title><?php wp_title(''); ?></title>
 		
-		<!-- Google Chrome Frame for IE -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		
-		<!-- mobile meta (hooray!) -->
 		<meta name="HandheldFriendly" content="True">
 		<meta name="MobileOptimized" content="320">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		
-		<!-- icons & favicons (for more: http://themble.com/support/adding-icons-favicons/) -->
 		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 				
   		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 		
-		<!-- wordpress head functions -->
 		<?php wp_head(); ?>
-		<!-- end of wordpress head -->
 		
 	</head>
 	
@@ -47,13 +41,4 @@
 				<a href="#" id="pull">Menú</a>
 			</nav>
 		</header>
-
-
 		<div id="container">
-
-
-
-
-
-
-
