f11f2609d310183e4eb78a9432b997b5de97d926
Claudio Noguera <claudio.noguera@gmail.com>
2013-06-13 01:31:52 +0200

    modificar la plantilla de single.php --> ahora no tiene sidebar y está centrada

--- single.php
+++ single.php
@@ -4,7 +4,7 @@
 
 				<div id="inner-content" class="wrap clearfix">
 			
-					<div id="main" class="ninecol fisrt single clearfix" role="main">
+					<div id="main" class="ninecol first single clearfix" role="main">
 						<?php
 						global $wp_query;
     					$list_of_posts = $wp_query;
