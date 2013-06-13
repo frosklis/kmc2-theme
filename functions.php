c496114660b6e7a06277263943e3eddb88254194
Claudio Noguera <claudio.noguera@gmail.com>
2013-06-13 23:28:45 +0200

    poner alignleft y todo eso bien

--- functions.php
+++ functions.php
@@ -206,7 +206,7 @@ function display_posts ($list_of_posts = null, $resumen = false) {
                     if ( has_post_thumbnail() ) {
                         the_post_thumbnail("medium",array('class' => 'alignleft'));
                     } else { 
-                        echo_first_image(get_the_ID();
+                        echo_first_image(get_the_ID());
                     }
                     the_excerpt();
                 } else {
