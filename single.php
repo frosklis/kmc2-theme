2649f5eac86e6d05f1a1217d58b59fe1a7a25640
Claudio Noguera <claudio.noguera@gmail.com>
2013-06-15 21:13:42 +0200

    probar breadcrumbs en single.php

--- single.php
+++ single.php
@@ -5,6 +5,10 @@
 				<div id="inner-content" class="wrap clearfix">
 			
 					<div id="main" class="ninecol first single clearfix" role="main">
+						<?php 
+						if ( function_exists('yoast_breadcrumb') ) {
+							yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+						} ?>
 						<?php
 						global $wp_query;
     					$list_of_posts = $wp_query;
