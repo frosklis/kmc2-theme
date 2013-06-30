1ad724ab67c77328dc5282c9ef25c58405facf19
Claudio Noguera <claudio.noguera@gmail.com>
2013-06-30 20:01:18 +0200

    añadir searchform.php y traducir algún texto

---
+++ searchform.php
@@ -0,0 +1,3 @@
+<form role="search" method="get" id="searchform" action="<?php bloginfo('siteurl'); ?>">
+    <input type="text" value="" name="s" id="s" placeholder="<?php _e('Search this blog...', kmc2theme); ?>">
+</form>
