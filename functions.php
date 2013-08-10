d63ba6d15adb73578f9a3e3cd7ef09cccb801be2
Claudio Noguera <claudio.noguera@gmail.com>
2013-08-10 13:05:46 +0200

    actualizar traducciones al español que quedaban pendientes

--- functions.php
+++ functions.php
@@ -116,7 +116,7 @@ function kmc2_register_sidebars() {
     register_sidebar(array(
         'id' => 'category_widgets',
         'name' => __('Category page widgets', 'kmc2theme'),
-        'description' => __('These widgets go right below the navbar in the catogry pages', 'kmc2theme'),
+        'description' => __('These widgets go right below the navbar in the category pages', 'kmc2theme'),
         'before_widget' => '<div id="%1$s" class="widget %2$s">',
         'after_widget' => '</div>',
         'before_title' => '<h4 class="widgettitle">',
