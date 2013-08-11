7ac61103b88b29370ad9402073ac45749d97806a
Claudio Noguera <claudio.noguera@gmail.com>
2013-08-12 00:40:38 +0200

    quitar class, width y height de las imágenes insertadas

--- functions.php
+++ functions.php
@@ -76,6 +76,15 @@ for the 600 x 100 image:
 You can change the names and dimensions to whatever
 you like. Enjoy!
 */
+add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 );
+add_filter( 'image_send_to_editor', 'remove_width_attribute', 10 );
+ 
+function remove_width_attribute( $html ) {
+    $html = preg_replace('/(width|height)=\"\d*\"\s/', "", $html );
+    $html = preg_replace('/class=".*?"/', "", $html );
+    $html = preg_replace('/alt=".*?"/', "", $html );
+    return $html;
+}
 
 /************* ACTIVE SIDEBARS ********************/
 
