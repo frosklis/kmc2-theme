f7e3502fef7ba0cf6ab4d51c8fa18e8cc259a130
Claudio Noguera <claudio.noguera@gmail.com>
2013-09-16 23:19:51 +0200

    fixes #8 add a filter so that a custom image_downsize function is called. It may create a bug if its execution takes more than 30 seconds.

--- library/gallery.php
+++ library/gallery.php
@@ -1,4 +1,78 @@
 <?php
+add_filter( 'image_downsize', 'kmc2_image_downsize', 10, 3 );
+
+// modified from https://gist.github.com/Viper007Bond/2785428
+ 
+function kmc2_image_downsize( $existing_data, $attachment_id, $size ) {
+
+	global $_wp_additional_image_sizes;
+
+	// Let WordPress handle named thumbnail sizes
+	if ( ! is_array( $size ) ) {
+		$size = $_wp_additional_image_sizes[$size];
+	}
+
+	// Safety
+	$size = array_map( 'absint', $size );
+ 
+	// Get the path to the fullsize image
+	$fullsize_path = get_attached_file( $attachment_id );
+ 
+	// Split it into parts
+	$fullsize_info = pathinfo( $fullsize_path );
+ 
+	// Create the thumbnail filename
+	// Is there a helper function for all of this? I couldn't find one.
+	$thumbnail_filename = str_replace( ".{$fullsize_info['extension']}", "-{$size[0]}x{$size[1]}.{$fullsize_info['extension']}", $fullsize_info['basename'] );
+ 
+	$thumbnail_path = $fullsize_info['dirname'] . '/' . $thumbnail_filename;
+ 
+	// If the thumbnail already exists
+	if ( file_exists( $thumbnail_path ) ) {
+ 
+		// Create the URL to the thumbnail by taking the fullsize image
+		// and replacing it's filename with the thumbnail filename
+		$thumbnail_url = str_replace( $fullsize_info['basename'], $thumbnail_filename, wp_get_attachment_url( $attachment_id ) );
+	}
+ 
+	// Okay, thumbnail doesn't exist. Make it!
+	else {
+ 
+		// Have to crop so that width/height is exact and findable in the future.
+		$new_thumbnail_path="";
+		$image = wp_get_image_editor( $fullsize_path );
+		if ( ! is_wp_error( $image ) ) {
+		    $image->resize( $size["width"], $size["height"], $size["crop"] );
+		    // $image->save( 'new_image.jpg' );
+		    $new_thumbnail_path = $image->save();
+		}
+
+		$new_thumbnail_path = $new_thumbnail_path["path"];
+
+
+
+		if ( is_wp_error( $new_thumbnail_path ) )
+			return $existing_data;
+ 
+		// Get the thumbnail path parts, specifically the filename
+		// Yeah, we created it above but let's be absolutely sure it's correct
+		$new_thumbnail_info = pathinfo( $new_thumbnail_path );
+ 
+		$thumbnail_url = str_replace( $fullsize_info['basename'], $new_thumbnail_info['basename'], wp_get_attachment_url( $attachment_id ) );
+	}
+ 
+	return array(
+		$thumbnail_url, // URL
+		$size[0],       // Width
+		$size[1],       // Height
+		true,           // is_intermediate, i.e. exact size or will it be resized via HTML?
+	);
+}
+
+
+
+
+
 remove_shortcode('gallery');
 add_shortcode('gallery', 'kmc2_gallery_shortcode');
 //add_filter( 'use_default_gallery_style', '__return_false' );
