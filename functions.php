1cf8f262b5f80f5ebf3b2fc9666b7c822aa8d155
Claudio Noguera <claudio.noguera@gmail.com>
2013-10-13 05:41:15 +0200

    all scripts to the footer

--- functions.php
+++ functions.php
@@ -232,10 +232,11 @@ function kmc2_scripts_and_styles() {
         }
 
         //adding scripts file in the footer
+        // wp_register_script( $handle, $src, $deps, $ver, $in_footer );
         wp_register_script( 'kmc2-js', get_stylesheet_directory_uri() . '/library/js/scripts.js', array( 'jquery' ), '', true );
         wp_register_script( 'masonry', get_stylesheet_directory_uri() . '/library/js/masonry.pkgd.min.js', array( 'jquery' ), '', true );
-        wp_register_script( 'kmc2-gallery', get_stylesheet_directory_uri() . '/library/js/gallery.js', array( 'masonry' ), '', false );
-        wp_register_script( 'flexslider', get_stylesheet_directory_uri() . '/library/js/jquery.flexslider-min.js', array( 'jquery' ), '', false );
+        wp_register_script( 'kmc2-gallery', get_stylesheet_directory_uri() . '/library/js/gallery.js', array( 'masonry' ), '', true );
+        wp_register_script( 'flexslider', get_stylesheet_directory_uri() . '/library/js/jquery.flexslider-min.js', array( 'jquery' ), '', true );
 
         wp_localize_script('kmc2-js', 'image_sizes_vars', array(
                 'thumbnail' => array(150,150),
