68c5a63f46174fb7a874a89896c40d3c7893742d
frosklis <claudio.noguera@gmail.com>
2014-01-23 15:27:46 +0100

    Cleaning
    The display_posts function was a mess. It needed some tidying up.
    
    It may need further simplyfying.

--- library/display_posts.php
+++ library/display_posts.php
@@ -1,5 +1,11 @@
 <?php
-// Dibujar los posts
+// 
+// Display the posts
+// A lot of options can be passed as keys to the $args array
+// return values:
+// 1 --> everything went fine
+// -1 --> no_posts
+//
 function display_posts ($args) {
     $list_of_posts = isset($args["list_of_posts"]) ? $args["list_of_posts"] : null;
     $summary = isset($args["summary"]) ? $args["summary"] : false;
@@ -10,10 +16,35 @@ function display_posts ($args) {
 
     $tiles = isset($args["tiles"]) ? $args["tiles"] : false;
 
-    if($tiles) {
-        ?><div class="article-list">
-        <?php
-        while ($list_of_posts->have_posts()) : $list_of_posts->the_post(); 
+	
+	// Container for the article list
+	$articles  = $single ? "article-single" : "article-list";
+	echo ('<div class="' . $articles . '">');
+	
+	// If there are no posts, display an error message to the user and return false
+	if (!$list_of_posts->have_posts()) { ?>
+		<article id="post-not-found" class="hentry clearfix">
+            <header class="article-header">
+                <h1><?php _e("Oops, Post Not Found!", "kmc2theme"); ?></h1>
+            </header>
+            <section class="entry-content">
+                <p><?php _e("Uh Oh. Something is missing. Try double checking things.", "kmc2theme"); ?></p>
+            </section>
+            <footer class="article-footer">
+                <p><?php _e("This is the error message in the display_posts function.", "kmc2theme"); ?></p>
+            </footer>
+        </article>
+		<?php
+		echo ('</div>'); // Close article list div
+		return -1;
+	}
+	
+	// If we're here, we have posts
+	while ($list_of_posts->have_posts()) {
+		$list_of_posts->the_post(); // update wordpress variables
+		
+		// If what we have are tiles
+		if($tiles) {
             $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), "width_260");
             $thumbnail = $thumbnail[0];
         ?>
@@ -26,21 +57,12 @@ function display_posts ($args) {
                     printf('<time class="updated" datetime="%1$s" pubdate>%2$s</time></p></div>', 'kmc2theme', get_the_time('Y-m-j'), get_the_time(get_option('date_format'))); ?>
                 </div>
             </a>
-        <?php endwhile;
-
-        return true;
-    }
-
-    if ($single) { ?>
-        <div class="article-single"> 
-        <?php
-    } else { ?>
-        <div class="article-list"> 
-        <?php
-    }
-        
-        
-    if ($list_of_posts->have_posts()) : while ($list_of_posts->have_posts()) : $list_of_posts->the_post(); ?>
+        <?php 
+			continue;
+		} // if tiles
+		
+		// No tiles
+		?>
 
         <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
         
@@ -52,31 +74,34 @@ function display_posts ($args) {
                     <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
                     <?php } ?>
                 <div class="byline"><?php
-                // printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'kmc2theme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), kmc2_get_the_author_posts_link(), get_the_category_list(', '));
 
-                // Edit the entry
-                edit_post_link(__('Edit', 'kmc2theme'),'<div class="info"><span class="icon-pencil"></span>','</div>');
+					// Edit the entry
+					edit_post_link(__('Edit', 'kmc2theme'),'<div class="info"><span class="icon-pencil"></span>','</div>');
 
-                // Date
-                echo('<div class="info"><span class="icon-calendar"></span> ');
-                printf('<time class="updated" datetime="%1$s" pubdate>%2$s</time></div>', 'kmc2theme', get_the_time('Y-m-j'), get_the_time(get_option('date_format')));
-                // Author
-                echo('<div class="info"><span class="icon-user"></span> ');
-                echo(kmc2_get_the_author_posts_link());
-                echo('</div>');
+					// Date
+					echo('<div class="info"><span class="icon-calendar"></span> ');
+					printf('<time class="updated" datetime="%1$s" pubdate>%2$s</time></div>', 'kmc2theme', 
+						get_the_time('Y-m-j'), 
+						//get_the_time(get_option('date_format'))
+						date_i18n(get_option('date_format') ,get_the_time('Y-m-j')) // Localized date
+						);
+					// Author
+					echo('<div class="info"><span class="icon-user"></span> ');
+					echo(kmc2_get_the_author_posts_link());
+					echo('</div>');
 
-                // Category
-                if (get_the_category()) {
-                    echo('<div class="info"><span class="icon-folder-close-alt"></span> ');
-                    the_category(' <span class="icon-folder-close-alt"></span> ', ', ');
-                    echo('</div>');
-                }
-                
-                // Tags
-                the_tags('<div class="info"><span class="icon-tag"></span> ', ', ', '</div>');
+					// Category
+					if (get_the_category()) {
+						echo('<div class="info"><span class="icon-folder-close-alt"></span> ');
+						the_category(' <span class="icon-folder-close-alt"></span> ', ', ');
+						echo('</div>');
+					}
+					
+					// Tags
+					the_tags('<div class="info"><span class="icon-tag"></span> ', ', ', '</div>');
 
-                // printf(__(' <time class="updated" datetime="%1$s" pubdate>%2$s</time> <span class="icon-user"></span> <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'kmc2theme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), kmc2_get_the_author_posts_link(), get_the_category_list(', '));
-                ?></div>
+
+                ?></div><!-- byline class -->
 
                 <?php wp_link_pages('before=<div id="page-links">&after=</div>'); ?>
         
@@ -119,8 +144,11 @@ function display_posts ($args) {
             
         </article> <!-- end article -->
     <?php
-    if ($prev_next_links) {
-        echo('</div><!-- article-list -->');
+		
+	}
+
+    // Navigation links to previous and next articles        
+    if ($single && $prev_next_links) {
         echo('<div class="wp-prev-next">'); 
             previous_post_link('<div class="prev-link">%link</div>', 
                 '<img src="' . get_stylesheet_directory_uri() . '/images/icons/arrow_left_16.png"  width="16" height="16">' . "%title");
@@ -128,38 +156,14 @@ function display_posts ($args) {
                 "%title" . '<img src="' . get_stylesheet_directory_uri() . '/images/icons/arrow_right_16.png"  width="16" height="16">'); 
         echo('</div>');
     }
-    ?>
-    <?php endwhile; 
-    if (!$prev_next_links) {
-        echo('</div>');
-    }
-        if (function_exists('kmc2_page_navi')) { ?>
-            <?php kmc2_page_navi(); ?>
-        <?php } else { ?>
-            <nav class="wp-prev-next">
-                <ul class="clearfix">
-                    <li class="prev-link"><?php next_posts_link(__('&laquo; Older Entries', "kmc2theme")) ?></li>
-                    <li class="next-link"><?php previous_posts_link(__('Newer Entries &raquo;', "kmc2theme")) ?></li>
-                </ul>
-            </nav>
-        <?php } ?>      
-
-    <?php else : ?>
+	
+	if (!$single) { 
+		kmc2_page_navi();
+	} 
+		
+	// final steps
+	echo ('</div>'); // Close article list div
+	return 1;
     
-        <article id="post-not-found" class="hentry clearfix">
-            <header class="article-header">
-                <h1><?php _e("Oops, Post Not Found!", "kmc2theme"); ?></h1>
-            </header>
-            <section class="entry-content">
-                <p><?php _e("Uh Oh. Something is missing. Try double checking things.", "kmc2theme"); ?></p>
-            </section>
-            <footer class="article-footer">
-                <p><?php _e("This is the error message in the display_posts function.", "kmc2theme"); ?></p>
-            </footer>
-        </article>
-
-    <?php endif; ?>
-
-
-<?php
+		
 }
