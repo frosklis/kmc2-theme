9e94a23a230670ad91f887c5438c244059b48855
Claudio Noguera <claudio.noguera@gmail.com>
2014-02-09 21:20:54 +0100

    content not optional, always appears

--- home.php
+++ home.php
@@ -2,27 +2,23 @@
 
 <div id="content">
 	<div id="inner-content" class="clearfix">
-		<?php 
-		if ( is_active_sidebar( 'homepage_widgets' ) ) dynamic_sidebar( 'homepage_widgets' ); 
-		else {
-			?>
-			<div id="main" class="twelvecol clearfix" role="main">
-			    <?php 
-			    global $wp_query;
-				$list_of_posts = $wp_query;
-				$args = array(
-		            'list_of_posts' => $list_of_posts,
-		            'summary' => true,
-		            'tiles' => true,
-		            'pages' => false,
-		        );
-				display_posts($args);
-				?>
-	
-		    </div> 
 		<?php
-		}
+		if ( is_active_sidebar( 'homepage_widgets' ) ) dynamic_sidebar( 'homepage_widgets' );
 		?>
+		<div id="main" class="twelvecol clearfix" role="main">
+		    <?php
+		    global $wp_query;
+			$list_of_posts = $wp_query;
+			$args = array(
+	            'list_of_posts' => $list_of_posts,
+	            'summary' => true,
+	            'tiles' => true,
+	            'pages' => false,
+	        );
+			display_posts($args);
+			?>
+
+	    </div>
 	</div> <!-- end #inner-content -->
 
 </div> <!-- end #content -->
